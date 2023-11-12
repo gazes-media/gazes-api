037f2313925b311f5234fe060b55e1e79463e03b
garder500 <65552225+garder500@users.noreply.github.com>
2023-11-12 14:47:33 +0100

    Update AnimesRss.route.ts

--- src/route/animes/AnimesRss.route.ts
+++ src/route/animes/AnimesRss.route.ts
@@ -28,11 +28,12 @@ export class AnimesRssRoute extends Route {
         });
 
         let rss = `<?xml version="1.0" encoding="UTF-8"?>
-            <rss version="2.0" xmlns:media="http://search.yahoo.com/mrss/">
+            <rss version="2.0" xmlns:media="http://search.yahoo.com/mrss/" xmlns:atom="http://www.w3.org/2005/Atom">
         <channel>
+            <atom:link href="https://api.gazes.fr/anime/animes/rss" rel="self" type="application/rss+xml" />
             <title>Gaze RSS Feed</title>
-            <link>https://deril-fr.github.io/</link>
-            <description>Latest anime releases from Gaze</description>
+            <link>https://gazes.fr/</link>
+            <description>Latest anime releases from Gazes</description>
             <language>fr-fr</language>
             <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
             <pubDate>${new Date().toUTCString()}</pubDate>
@@ -40,11 +41,12 @@ export class AnimesRssRoute extends Route {
             ${animesFiltered
                 .map((anime) => {
                     return `<item>
+                <guid>${(new Date(anime.pubDate)).valueOf()}</guid>
                 <title>${anime.title}</title>
                 <link>${anime.link}</link>
                 <description>${anime.description}</description>
                 <pubDate>${anime.pubDate}</pubDate>
-                <media:thumbnail url="${anime.image}" type="image/jpeg" medium="image" width="300" height="300" xmlns:media="http://search.yahoo.com/mrss/" />
+                <media:thumbnail url="${anime.image}" medium="image" width="300" height="300" xmlns:media="http://search.yahoo.com/mrss/" />
             </item>\n`;
                 })
                 .join("")}
