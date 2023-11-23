7dd9a5b7b9c11212ee5dcca4fee0b4045ab1e738
l31 <geloocg@gmail.com>
2023-11-23 07:17:17 +0100

    summary

--- src/route/Route.ts
+++ src/route/Route.ts
@@ -1,7 +1,13 @@
 import { HTTPMethods, RouteHandlerMethod } from "fastify";
 
+// ceci est un
+// test en plusieurs blocs
+// pour uniquement
+// essayer de commit une portion
+
 export abstract class Route {
     public abstract url: string;
     public abstract method: HTTPMethods;
     public abstract handler: RouteHandlerMethod;
 }
+
